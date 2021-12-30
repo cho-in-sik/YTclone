8a9f54415a5bf017c380b9b4d5afabdc33292da3
조인식 <imsif@naver.com>
2021-12-30 14:29:52 +0000

    learning req/res

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 import express from "express";
 
+const PORT = 3000;
+
 const app = express();
 
-app.listen(5050, () => console.log("Server listening on port 5050"));
+app.get("/",(req,res) => {return res.send("<h1>I still love you</h1>")});
+app.get("/login", (req,res) => {return res.send("Login here.")});
+
+const handleListening= () => console.log(`Server listening on port https://yt-clone-gnylx.run.goorm.io/`);
+
+app.listen(PORT, handleListening);
