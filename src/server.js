bbaf385ee0b52560493a52a3be2e86076ae2ce83
조인식 <imsif@naver.com>
2021-12-31 11:11:58 +0000

    2

--- src/server.js
+++ src/server.js
@@ -4,8 +4,16 @@ const PORT = 3000;
 
 const app = express();
 
-app.get("/",(req,res) => {return res.send("<h1>I still love you</h1>")});
-app.get("/login", (req,res) => {return res.send("Login here.")});
+const logger = (req,res,next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+app.get("/",logger,handleHome);
 
 const handleListening= () => console.log(`Server listening on port https://yt-clone-gnylx.run.goorm.io/`);
 
