d719660722b79c890d6066a15e68cdd18a145e40
조인식 <imsif@naver.com>
2022-01-05 13:21:26 +0000

    .

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,33 @@
-export const trending = (req,res) => res.render("home",{pageTilte:"Home"});
+
+export const trending = (req,res) => {
+    const videos = [
+     {
+      title:"First Vidoe",
+      rating:5,
+      comments:2,
+      createdAt: "2 minites age",
+      views:59,
+      id:1   
+     },
+     {
+      title:"Second Vidoe",
+      rating:5,
+      comments:2,
+      createdAt: "2 minites age",
+      views:59,
+      id:1   
+     },
+     {
+      title:"Third Vidoe",
+      rating:5,
+      comments:2,
+      createdAt: "2 minites age",
+      views:59,
+      id:1   
+     },
+    ];
+    res.render("home",{pageTilte:"Home", videos});
+};
 export const see = (req,res) => res.render("watch");
 export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send("Search");
