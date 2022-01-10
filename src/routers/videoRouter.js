7d9d7b3efdf84b9778844ae556fb938468026579
조인식 <imsif@naver.com>
2022-01-10 13:34:45 +0000

    11

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
 import {watch, getEdit,postEdit,upload,deleteVideo} from "../controllers/videoController";
 
-const videoRouther = express.Router();
+const videoRouter = express.Router();
 
 
-videoRouther.get("/:id(\\d+)",watch);
-videoRouther.get("/:id(\\d+)/edit",getEdit);
-videoRouther.post("/:id(\\d+)/edit",postEdit);
+videoRouter.get("/:id(\\d+)",watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 
-export default videoRouther
+
+export default videoRouter
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
-import videoRouther from "./routers/videoRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 3000;
 
@@ -11,8 +11,9 @@ const app = express();
 app.set("view engine","pug");
 app.set("views",process.cwd() + "/src/views");
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 app.use("/",globalRouter);
-app.use("/videos",videoRouther);
+app.use("/videos",videoRouter);
 app.use("/users",userRouter);
 
 
